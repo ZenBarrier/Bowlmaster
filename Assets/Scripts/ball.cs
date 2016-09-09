5ec89c969691ec8759de9e6ef7c44284936140d7
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-09 16:54:33 -0400

    Ball with physics created

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+    public float launchSpeed;
+
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = this.GetComponent<Rigidbody>();
+        audioSource = this.GetComponent<AudioSource>();
+
+        LaunchBall();
+    }
+
+    public void LaunchBall()
+    {
+        rigidBody.velocity = Vector3.forward * launchSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Lane")
+        {
+            audioSource.Play();
+        }
+    }
+}
