23e299b34ee7226fc8a63f74db914c6c319e0d40
ZenBarrier <anthony.j.barrera@gmail.com>
2016-09-13 16:09:14 -0400

    Camera control. follows ball

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    public Ball ball;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        ball = FindObjectOfType<Ball>();
+        offset = transform.position - ball.transform.position;
+        this.transform.position = ball.transform.position + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float zPos = this.transform.position.z;
+        if (zPos < 1829f)
+        {
+            this.transform.position = ball.transform.position + offset;
+        }
+    }
+}
